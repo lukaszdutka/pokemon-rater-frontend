9ec133a8c7967e51d8604b04b5c065f5da2e245b
lukaszdutka <lukaszdutka2@gmail.com>
2024-09-21 22:28:29 +0200

    add styling

--- src/App.js
+++ src/App.js
@@ -16,12 +16,16 @@ const App = () => {
   };
 
   return (
-    <div>
-      <nav>
-        <ul>
-          <li onClick={() => setCurrentPage('home')}>Home</li>
-          <li onClick={() => setCurrentPage('leaderboard')}>Leaderboard</li>
+    <div className="bg-gray-800 min-h-screen text-white">
+      <nav className="flex justify-between items-center p-4">
+        <ul className="flex space-x-4">
+          <li className="cursor-pointer" onClick={() => setCurrentPage('home')}>Home</li>
+          <li className="cursor-pointer" onClick={() => setCurrentPage('leaderboard')}>Leaderboard</li>
         </ul>
+        <div className="flex space-x-4">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">Button1</button>
+          <button className="bg-blue-500 text-white px-4 py-2 rounded">Button2</button>
+        </div>
       </nav>
       {renderPage()}
     </div>
--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
 const Leaderboard = () => {
@@ -13,22 +13,26 @@ const Leaderboard = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Leaderboard</h1>
-      <table>
+    <div className="flex flex-col items-center p-8">
+      <h1 className="text-4xl font-bold mb-6">Leaderboard</h1>
+      <table className="table-auto border-collapse border border-gray-300">
         <thead>
         <tr>
-          <th>Image</th>
-          <th>Name</th>
-          <th>Rating</th>
+          <th className="border px-4 py-2">Image</th>
+          <th className="border px-4 py-2">Name</th>
+          <th className="border px-4 py-2">Rating</th>
         </tr>
         </thead>
         <tbody>
         {pokemonList.map(pokemon => (
           <tr key={pokemon.id}>
-            <td><img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`} alt={pokemon.name} /></td>
-            <td>{pokemon.name}</td>
-            <td>{pokemon.rating}</td>
+            <td className="border px-4 py-2">
+              <img className="w-24 h-24"
+                   src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`}
+                   alt={pokemon.name}/>
+            </td>
+            <td className="border px-4 py-2 text-center">{pokemon.name}</td>
+            <td className="border px-4 py-2 text-center">{pokemon.rating}</td>
           </tr>
         ))}
         </tbody>
