7d1bab31fb5216e544a88907b613f7132ae090de
lukaszdutka <lukaszdutka2@gmail.com>
2024-09-21 22:06:34 +0200

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [question, setQuestion] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('https://poke-backend.drogaprogramisty.site/question')
+      .then(response => {
+        setQuestion(response.data);
+        setLoading(false);
+      });
+  }, []);
+
+  const handleAnswer = (winnerPokemonId) => {
+    axios.post('/answer', {
+      id: question.id,
+      winnerPokemonId: winnerPokemonId
+    }).then(() => {
+      // Optionally, reload question or indicate success
+    });
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello vercel!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign: 'center', marginTop: '50px'}}>
+      <h1>Which one is cooler?</h1>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <div style={{margin: '0 20px'}}>
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${question.firstPokemonId}.png`} alt="First Pokemon"/>
+          <button onClick={() => handleAnswer(question.firstPokemonId)}>This one is cooler</button>
+        </div>
+        <div style={{margin: '0 20px'}}>
+          <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${question.secondPokemonId}.png`} alt="Second Pokemon"/>
+          <button onClick={() => handleAnswer(question.secondPokemonId)}>This one is cooler</button>
+        </div>
+      </div>
     </div>
   );
 }
