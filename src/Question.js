41ebcfd189fcd2b599ac88ff2e48eadfb4bde7f1
lukaszdutka <lukaszdutka2@gmail.com>
2024-12-04 16:53:38 +0100

    app in progress.

--- src/Question.js
+++ src/Question.js
@@ -38,7 +38,8 @@ function Question() {
 
   return (
     <div className="bg-gray-800 text-center py-12">
-      <h1 className="text-white text-2xl">Which one is cooler?</h1>
+      <h1 className="text-white text-3xl">Migration in progress. 18$/month for simple backend is outrageous. AWS, shame on you!</h1>
+      {/*<h1 className="text-white text-2xl">Which one is cooler?</h1>*/}
       <div className="flex justify-center mt-8 p-4">
         <div className="border border-gray-500 rounded p-4 mx-2">
           <h2 className="text-white text-xl">{capitalizeFirstLetter(question.firstPokemonName)}</h2>
